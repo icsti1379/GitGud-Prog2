c0274dd4eaba6970abfa80d1448a527adeaa86c6
icsti1379 <it.icsti@googlemail.com>
2016-12-05 15:09:01 +0100

    Added a input manager

---
+++ GitGud/GitGud-Prog2/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGudP2
+{
+    class InputManager
+    {   
+       /* public KeyIsPressed()
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+            {
+                kip.CurrentState = CharacterState.MovingRight;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            {
+                kip.CurrentState = CharacterState.MovingLeft;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            {
+                kip.CurrentState = CharacterState.MovingUp;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            {
+                kip.CurrentState = CharacterState.MovingDown;
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+            {
+                window.Close();
+            }
+        }*/
+    }
+}
+       
