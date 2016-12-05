43558ebf8f6c5ffd99c0a799b050424cfb57a256
icsti1379 <it.icsti@googlemail.com>
2016-12-05 15:04:06 +0100

    Add StateMachine with states

---
+++ GitGud/GitGud-Prog2/States/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
+
+namespace SFML_Prog2_Gruppe1.States
+{
+    public class MainMenu : State
+    {
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            ProjectRenderWindow.GetRenderWindowInstance().Clear(Color.Red);
+        }
+
+        public override void Initialize()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override GameStates Update()
+        {
+            return GameStates.MainMenuState;
+            //throw new NotImplementedException();
+        }
+    }
+}
