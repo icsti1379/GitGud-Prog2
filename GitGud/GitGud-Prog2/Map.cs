34f8364ae947c5703ed7f2d293034e7a965805c4
icsti1379 <it.icsti@googlemail.com>
2016-12-05 15:10:03 +0100

    Add a map class for implement the tilemap

---
+++ GitGud/GitGud-Prog2/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System.IO;
+
+namespace GitGudP2
+{
+    class Map
+    {
+        Sprite[,] tiles;
+        int mapWidth = 100;
+        int mapHeight = 100;
+
+        public Map()
+        {
+            int tilemapWidth = 21;
+            int tilemapHeight = 23;
+            int tileSize = 32;
+
+            Texture texture = new Texture("Maps/terrain.png");
+            Sprite[] tilemap = new Sprite[tilemapWidth * tilemapHeight];
+
+            for(int y = 0; y < tilemapHeight; y++)
+            {
+                for(int x = 0; x < tilemapWidth; x++)
+                {
+                    IntRect rect = new IntRect(x * tileSize, y * tileSize, tileSize, tileSize);
+                    tilemap[(y * tilemapWidth) + x] = new Sprite(texture, rect);
+                }
+            }
+
+            tiles = new Sprite[mapWidth, mapHeight];
+            StreamReader reader = new StreamReader("Maps/test.csv");
+
+            for(int y = 0; y < mapHeight; y++)
+            {
+                string line = reader.ReadLine();
+                string[] items = line.Split(',');
+
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    int id = Convert.ToInt32(items[x]);
+                    tiles[x , y] = new Sprite(tilemap[id]);
+                    tiles[x, y].Position = new Vector2f(tileSize * x, tileSize * y);
+                }
+            }
+
+            reader.Close();
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            for(int y = 0; y < mapHeight; y++)
+            {
+                for(int x = 0; x < mapWidth; x++)
+                {
+                    window.Draw(tiles[x , y]);
+                }
+            }
+        }
+    }
+}
