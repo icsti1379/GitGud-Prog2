233cc1c36580ff67850c6a5b3907fc604978b208
icsti1379 <it.icsti@googlemail.com>
2016-12-08 09:52:14 +0100

    Uncomment unfinished states

--- GitGud/GitGud-Prog2/States/GamePlay.cs
+++ GitGud/GitGud-Prog2/States/GamePlay.cs
@@ -1,57 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-using SFML;
-using SFML.System;
-using SFML.Window;
-using SFML.Graphics;
-using SFML.Audio;
+//using SFML;
+//using SFML.System;
+//using SFML.Window;
+//using SFML.Graphics;
+//using SFML.Audio;
 
-namespace SFML_Prog2_Gruppe1.States
-{
-    public class GamePlay : State
-    {
-        World world;
-        Player player;
-        QuestNPC questNPC;
-        EnemyNPC enemyNPC;
+//namespace SFML_Prog2_Gruppe1.States
+//{
+//    public class GamePlay : State
+//    {
+//        World world;
+//        Player player;
+//        QuestNPC questNPC;
+//        EnemyNPC enemyNPC;
 
-        public GamePlay()
-        {
-            world = new World();
-            player = new Player();
-            questNPC = new QuestNPC();
-            enemyNPC = new EnemyNPC();
-        }
+//        public GamePlay()
+//        {
+//            world = new World();
+//            player = new Player();
+//            questNPC = new QuestNPC();
+//            enemyNPC = new EnemyNPC();
+//        }
 
-        public override void Dispose()
-        {
-           // throw new NotImplementedException();
-        }
+//        public override void Dispose()
+//        {
+//           // throw new NotImplementedException();
+//        }
 
-        public override void Draw()
-        {
-            world.Draw();
-            player.Draw();
-            questNPC.Draw();
-            enemyNPC.Draw();
-            //throw new NotImplementedException();
-        }
+//        public override void Draw()
+//        {
+//            world.Draw();
+//            player.Draw();
+//            questNPC.Draw();
+//            enemyNPC.Draw();
+//            //throw new NotImplementedException();
+//        }
 
-        public override void Initialize()
-        {
-            //throw new NotImplementedException();
-        }
+//        public override void Initialize()
+//        {
+//            //throw new NotImplementedException();
+//        }
 
-        public override GameStates Update()
-        {
-            player.Update(world.CurrentRoom);
-            return GameStates.GamePlayState;
-            //throw new NotImplementedException();
-        }
+//        public override GameStates Update()
+//        {
+//            player.Update(world.CurrentRoom);
+//            return GameStates.GamePlayState;
+//            //throw new NotImplementedException();
+//        }
 
-    }
-}
+//    }
+//}
--- GitGud/GitGud-Prog2/States/State.cs
+++ GitGud/GitGud-Prog2/States/State.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-using SFML;
-using SFML.System;
-using SFML.Window;
-using SFML.Graphics;
-using SFML.Audio;
+//using SFML;
+//using SFML.System;
+//using SFML.Window;
+//using SFML.Graphics;
+//using SFML.Audio;
 
-namespace SFML_Prog2_Gruppe1
-{
-    public abstract class State
-    {
-        public abstract void Initialize();
+//namespace SFML_Prog2_Gruppe1
+//{
+//    public abstract class State
+//    {
+//        public abstract void Initialize();
 
-        public abstract GameStates Update();
+//        public abstract GameStates Update();
 
-        public abstract void Draw();
+//        public abstract void Draw();
 
-        public abstract void Dispose();
-    }
-}
+//        public abstract void Dispose();
+//    }
+//}
