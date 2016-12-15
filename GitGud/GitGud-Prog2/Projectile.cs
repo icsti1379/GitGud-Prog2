908a5bfe3da47e4b10d64b6b61c07d4d4560e967
Syoxx <npietrek13@googlemail.com>
2016-12-15 21:54:56 +0100

    small changes

--- GitGud/GitGud-Prog2/Projectile.cs
+++ GitGud/GitGud-Prog2/Projectile.cs
@@ -19,9 +19,22 @@ namespace GitGudP2
         int travelSpeedY;
         ProjectileCollisionHandling CollisionHandling;
 
-        public  Projectile (Vector2f originPos, Vector2f direction)
+        public Projectile (Vector2f originPos, Vector2f direction)
         {
+            this.projectilePos = originPos;
+            this.projectileDirection = direction;
 
+        }
+
+        public void Update(float deltaTime)
+        {
+            projectilePos.X += projectileDirection.X * deltaTime;
+            projectilePos.Y += projectileDirection.Y * deltaTime;
+
+            /*if (CollisionHandling.ProjectileCollision())
+             * TODO herrausfinden wie ich collision mit allen gegner überprüfe
+             * nach collision gegner + projectile entfernen, counter erhöhen 
+             */
         }
 
         public Vector2f ProjectilePos()
