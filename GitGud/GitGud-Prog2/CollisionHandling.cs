c9a7bcefc24013a5da060f3cfdca6fb824764f5e
Syoxx <npietrek13@googlemail.com>
2016-12-18 23:06:25 +0100

    small changes

--- GitGud/GitGud-Prog2/CollisionHandling.cs
+++ GitGud/GitGud-Prog2/CollisionHandling.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 
 namespace GitGudP2
