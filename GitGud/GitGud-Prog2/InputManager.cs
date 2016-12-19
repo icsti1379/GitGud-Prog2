c64c85dee157f446932e8ddf92f073affa0708fd
icsti1379 <it.icsti@googlemail.com>
2016-12-19 07:33:17 +0100

    Fixed some bugs

--- GitGud/GitGud-Prog2/InputManager.cs
+++ GitGud/GitGud-Prog2/InputManager.cs
@@ -7,31 +7,110 @@ using System.Threading.Tasks;
 
 namespace GitGudP2
 {
-    class InputManager
-    {   
-       /* public KeyIsPressed()
+    public class InputManager
+    {
+        private CharacterState CurrentState;
+
+        public void InputHandler()
         {
-            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+            this.CurrentState = CharacterState.None;
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right))
             {
-                kip.CurrentState = CharacterState.MovingRight;
+                this.CurrentState = CharacterState.MovingRight;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left))
             {
-                kip.CurrentState = CharacterState.MovingLeft;
+                this.CurrentState = CharacterState.MovingLeft;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W) || Keyboard.IsKeyPressed(Keyboard.Key.Up))
             {
-                kip.CurrentState = CharacterState.MovingUp;
+                this.CurrentState = CharacterState.MovingUp;
             }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S) || Keyboard.IsKeyPressed(Keyboard.Key.Down))
             {
-                kip.CurrentState = CharacterState.MovingDown;
+                this.CurrentState = CharacterState.MovingDown;
             }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Return))
+            {
+                
+            }
+
             if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
             {
-                window.Close();
+
             }
-        }*/
+        }
     }
 }
-       
+
+/*  
+     this.CurrentState = CharacterState.None;
+
+        if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+        {
+            this.CurrentState = CharacterState.MovingRight;
+        }
+        
+         
+
+
+
+  if (Keyboard.IsKeyPressed(Keyboard.Key.W) && !up)
+     {
+         vCharacterPosition.Y -= fCharacterVelocity;
+         bMovingUp = true;
+     }
+
+     if (Keyboard.IsKeyPressed(Keyboard.Key.S) && !down)
+     {
+         vCharacterPosition.Y += fCharacterVelocity;
+         bMovingDown = true;
+     }
+
+     if (Keyboard.IsKeyPressed(Keyboard.Key.A) && !left)
+     {
+         vCharacterPosition.X -= fCharacterVelocity;
+         bMovingLeft = true;
+     }
+
+     if (Keyboard.IsKeyPressed(Keyboard.Key.D) && !right)
+     {
+         vCharacterPosition.X += fCharacterVelocity;
+         bMovingRight = true;
+     }
+
+     vMousePosition = Mouse.GetPosition(window);
+
+     if (Mouse.IsButtonPressed(Mouse.Button.Left) && !bPressed)
+     {
+         Shoot = true;
+         bPressed = true;
+     }
+     else if (!Mouse.IsButtonPressed(Mouse.Button.Left))
+         bPressed = false;
+ }*/
+
+/* Mouse direction
+    protected void PlayerRotation()
+    {
+        // Calculating Mouse Position using the Character Position as Origin
+        vMousePositionFromPlayer = (Vector2i)CharacterPosition + new Vector2i(25,25) - Input.vMousePosition;
+
+
+        // Calculating Angle of the Mouse Position relative to the Character
+        iAngle = (float)Math.Acos(      (vMousePositionFromPlayer.X    *   0     +     vMousePositionFromPlayer.Y   *   1)  /
+                                        (Math.Sqrt(    Math.Pow(vMousePositionFromPlayer.X, 2)   +   Math.Pow(vMousePositionFromPlayer.Y, 2)   )        *       Math.Sqrt(    Math.Pow(0, 2)   +   Math.Pow(1, 2)    )    ));
+
+        iAngle = (iAngle / (float)Math.PI * 180);
+
+        if (vMousePositionFromPlayer.X > 0)
+            iAngle = 360 - iAngle;
+
+
+        // Rotating Character
+        sEntity.Origin = new Vector2f(25,25);
+        sEntity.Rotation = iAngle;
+    }*/
+
